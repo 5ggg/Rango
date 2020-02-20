4425cb188a9f52f7f83338325ebf06d1f588a5af
JiaLiang Song <56025832+5ggg@users.noreply.github.com>
2020-02-20 06:12:14 +0000

    chapter 7 content

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
+from django.shortcuts import redirect
 from django.http import HttpResponse
 
 # import the category model
 from rango.models import Category
 from rango.models import Page
 
+from rango.forms import CategoryForm
 
 
 
@@ -20,10 +22,9 @@ def index(request):
     # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     # it spend me 4 hours to find this bugs......
-
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
@@ -61,3 +62,26 @@ def show_category(request, category_name_slug):
     # Go render the responze and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
 
+
+def add_category(request):
+    form = CategoryForm()
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+    # Have we been provided with a valid form?
+    if form.is_valid():
+        # Save the new category to the database.
+        form.save(commit=True)
+        # Now that the category is saved, we could confirm this.
+        # For now, just redirect the user back to the index view.
+        return redirect('/rango/')
+    else:
+        # The supplied form contained errors -
+        # just print them to the terminal.
+        print(form.errors)
+
+    # Will handle the bad form, new, form, or no form supplied cases.
+    # Render the form with error mesaages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
